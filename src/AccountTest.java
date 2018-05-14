76a245a31bfda1a8dd17447a066ae613597587cd
sandeep155 <36008752+sandeep155@users.noreply.github.com>
2018-05-14 18:16:20 -0400

    Added account and driver class

--- src/AccountTest.java
+++ src/AccountTest.java
@@ -4,7 +4,7 @@ public class AccountTest {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        Account myAccount = new Account();
+        Account myAccount = new Account(name:"Sandeep");
 
         System.out.printf("Initial name is:%s%n%n", myAccount.getName());
 
